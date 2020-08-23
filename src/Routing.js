26abaee448751d700ddc48931dc0b38d284540a7
masham04 <66207787+masham04@users.noreply.github.com>
2020-08-23 18:33:43 +0500

    next update

--- src/Routing.js
+++ src/Routing.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import {
     BrowserRouter as Router,
-    Switch,Route,Link
-  } from "react-router-dom";
+    Switch,Route} from "react-router-dom";
   import {Home} from './components/Home';
   import {About} from './components/About';
   import {Product} from './components/Product';
